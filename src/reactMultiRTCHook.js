1b520a5e472944b66d63796e67cf79a1f11d89f2
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-12-12 17:54:36 +0900

    feat: 커스텀 훅 완성

--- src/reactMultiRTCHook.js
+++ src/reactMultiRTCHook.js
@@ -1,93 +1,87 @@
 import {
-    useCallback, useRef, useEffect, useState,
-  } from 'react';
-  import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+  useCallback, useRef, useEffect, useState,
+} from 'react';
   
-  import useSocket from 'src/reactSocketHook';
+import useSocket from './reactSocketHook';
+import { bindTrailingArgs } from './util';
+import roomSocketMessage from './constant';
+
+export const useLocalStream = () => {
+  const myStreamRef = useRef(null);
+  const myVideoRef = useRef(null);
+
+  const getLocalStream = useCallback(async () => {
+    myStreamRef.current = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    if (myVideoRef.current) myVideoRef.current.srcObject = myStreamRef.current;
+      myStreamRef.current
+      .getAudioTracks()
+      .forEach((track) => (track.enabled = !track.enabled));
+  }, []);
+
+  return [myStreamRef, myVideoRef, getLocalStream];
+};
   
-  export const useLocalStream = () => {
-    const myStreamRef = useRef(null);
-    const myVideoRef = useRef(null);
-  
-    const getLocalStream = useCallback(async () => {
-      myStreamRef.current = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-      if (myVideoRef.current) myVideoRef.current.srcObject = myStreamRef.current;
-        myStreamRef.current
-          .getAudioTracks()
-          .forEach((track) => (track.enabled = !track.enabled));
-    }, []);
-  
-    return [myStreamRef, myVideoRef, getLocalStream];
+export const useSetPeerConnection = ( setParticipants, myStreamRef ) => {
+  const peerConnectionConfig = {
+    iceServers: [
+      { urls: 'stun:stun.l.google.com:19302' },
+    ],
   };
   
-  export const useSetPeerConnection = ( setParticipants, myStreamRef ) => {
-    const peerConnectionConfig = {
-      iceServers: [
-        {
-          urls: 'stun:stun.l.google.com:19302',
-        },
-      ],
-    };
+  const handleIceEvent = ({ candidate }, payload) => {
+    if (!(payload.socket && candidate)) return;
+    payload.socket.emit(roomSocketMessage.ice, candidate, payload.socketId);
+  };
   
-    const handleIceEvent = ({ candidate }, payload) => {
-      if (!(payload.socket && candidate)) return;
-      payload.socket.emit(roomSocketMessage.ice, candidate, payload.socketId);
-    };
+  const handleTrackEvent = (data, payload) => {
+    setParticipants((oldParticipants) => oldParticipants
+      .filter((participant) => (participant.userDocumentId !== payload.userDocumentId))
+      .concat({
+        stream: data.streams[0],
+        userDocumentId: payload.userDocumentId,
+        socketId: payload.socketId,
+      }));
+  };
   
-    const handleTrackEvent = (data, payload) => {
-      setParticipants((oldParticipants) => oldParticipants
-        .filter((participant) => (participant.userDocumentId !== payload.userDocumentId))
-        .concat({
-          stream: data.streams[0],
-          userDocumentId: payload.userDocumentId,
-          mic: payload.mic,
-          socketId: payload.socketId,
-          isAnonymous: payload.isAnonymous,
-        }));
-    };
-  
-    const setPeerConnection = useCallback((participant, socket) => {
-      try {
+  const setPeerConnection = useCallback((participant, socket) => {
+    try {
         const peerConnection = new RTCPeerConnection(peerConnectionConfig);
-  
-        const handleIce = bindTrailingArgs(handleIceEvent, { socketId: participant.socketId, socket });
-        const handleTrack = bindTrailingArgs(handleTrackEvent, {
-          userDocumentId: participant.userDocumentId,
-          mic: participant.mic,
-          socketId: participant.socketId,
-          isAnonymous: participant.isAnonymous,
-          socket,
-        });
-  
+        const { userDocumentId, socketId } = participant;
+
+        const handleIce = bindTrailingArgs(handleIceEvent, { socketId, socket });
+        const handleTrack = bindTrailingArgs(handleTrackEvent, { userDocumentId, socketId });
+
         peerConnection.addEventListener('icecandidate', handleIce);
         peerConnection.addEventListener('track', handleTrack);
-  
+
         myStreamRef.current.getTracks().forEach((track) => {
           if (!myStreamRef.current) return;
           peerConnection.addTrack(track, myStreamRef.current);
         });
-  
+
         return peerConnection;
-      } catch (e) {
+    } catch (e) {
         console.error(e);
         return undefined;
-      }
-    }, []);
+    }
+  }, []);
   
-    return setPeerConnection;
-  };
+  return setPeerConnection;
+};
   
-  export const useRtc = () => {
+  /*
+    payload {
+        roomDocumentId,
+        userDocumentId,
+    }
+  */
+
+  export const useRTC = (payload, namespace) => {
     const peerConnectionsRef = useRef({});
     const [participants, setParticipants] = useState([]);
-    const isAnonymous = useRecoilValue(anonymousState);
-    const roomDocumentId = useRecoilValue(roomDocumentIdState);
-    const [user] = useRecoilState(userTypeState);
-    const socket = useSocket('/room');
+    const socket = useSocket(namespace);
     const [myStreamRef, myVideoRef, getLocalStream] = useLocalStream();
     const setPeerConnection = useSetPeerConnection(setParticipants, myStreamRef);
-    const setRoomView = useSetRecoilState(roomViewState);
-    const setToastList = useSetRecoilState(toastListSelector);
   
     useEffect(() => {
       if (!socket) return;
@@ -96,12 +90,10 @@ import {
         try {
           await getLocalStream();
           socket.emit(roomSocketMessage.join, {
-            roomDocumentId, userDocumentId: user.userDocumentId, socketId: socket.id, isAnonymous,
+            payload, socketId: socket.id,
           });
-          setToastList(toastMessage.roomEnterSuccess());
         } catch (error) {
-          setToastList(toastMessage.roomCreateDanger());
-          setRoomView('createRoomView');
+          console.error(error);
         }
       };
   
@@ -113,6 +105,7 @@ import {
             if (!myStreamRef.current) return;
             const peerConnection = setPeerConnection(participant, socket);
             if (!(peerConnection && socket)) return;
+
             peerConnectionsRef.current = { ...peerConnectionsRef.current, [participant.socketId]: peerConnection };
             const offer = await peerConnection.createOffer();
             peerConnection.setLocalDescription(offer);
@@ -124,14 +117,12 @@ import {
         }
       });
   
-      // eslint-disable-next-line @typescript-eslint/no-shadow
-      socket.on(roomSocketMessage.offer, async (offer, userDocumentId, socketId, isAnonymous) => {
+      socket.on(roomSocketMessage.offer, async (offer, userDocumentId, socketId) => {
         if (!myStreamRef.current) return;
-        const participant = {
-          userDocumentId, socketId, isAnonymous, mic: false,
-        };
+        const participant = { userDocumentId, socketId };
         const peerConnection = setPeerConnection(participant, socket);
         if (!(peerConnection && socket)) return;
+        
         peerConnectionsRef.current = { ...peerConnectionsRef.current, [socketId]: peerConnection };
         await peerConnection.setRemoteDescription(offer);
         const answer = await peerConnection.createAnswer();
@@ -178,6 +169,6 @@ import {
       };
     }, [socket, setPeerConnection]);
   
-    return [participants, setParticipants, myVideoRef, roomDocumentId, user, socket, myStreamRef];
+    return [participants, setParticipants, myVideoRef, socket, myStreamRef];
   };
   
